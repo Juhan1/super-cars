1ef78b6fdc661e5d2fd664eaec058c930b8bb8f2
Juhan1 <47728490+Juhan1@users.noreply.github.com>
2021-06-23 18:49:46 +1000

    Display search results
    Displays the search results as the search term is entered in a list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { CarComponent } from "./cars/car.component";
 import { SearchComponent } from "./search/search.component";
--- src/search/search.component.tsx
+++ src/search/search.component.tsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+import { findCars } from "../cars/car.api";
+import { Car } from "../cars/car";
+
 export const SearchComponent: React.FC = () => {
-    return <div>
-        <input className="search" type="search" />
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<Car[]>([]);
+
+  useEffect(() => {
+    async function fetchSearch() {
+      const request = await findCars(searchTerm);
+      //console.log(request[0].make);
+      setSearchResults(request);
+    }
+
+    fetchSearch();
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <input
+        className="search"
+        type="search"
+        placeholder="Search..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <ul>
+        {searchResults.map((result) => {
+          return (
+            <li key={result.make + result.model}>
+              {result.make} {result.model}
+              <br />
+              {result.price}
+            </li>
+          );
+        })}
+      </ul>
     </div>
-}
+  );
+};
